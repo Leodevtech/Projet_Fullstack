d8bcbad0b0d14c235616c64c47e2c0f7a1e107fd
Leodevtech <leodevtech@gmail.com>
2026-02-13 14:47:26 +0100

    patch app.js

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,13 @@ import authRoutes from "./routes/auth.routes.js";
 import "dotenv/config.js";
 import { authorize, authMiddleware } from "./middleware/auth.middleware.js";
 import cors from "cors";
+import credentialRoutes from "./routes/credential.routes.js";
 const app = express();
 const whiteList = ["http://localhost:5173"];
 
+// middleware pour parser le json
+
+// \\\\\\\\\\\\\\\\\\\\\\ ROUTES ////////////////// \\
 app.use(express.json());
 app.use(
   cors({
@@ -16,5 +20,5 @@ app.use("/api/auth", authRoutes);
 app.get("/", authMiddleware, authorize(["ADMIN", "MODERATOR"]), (req, res) =>
   res.send("Mon API fonctionne bien "),
 );
-
+app.use("/api/credentials", credentialRoutes);
 export default app;
